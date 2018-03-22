0ce7a229fcd010800ebff8384dc68d25bf783bba
BasharSaid <33035384+basharSaid@users.noreply.github.com>
2018-03-22 13:09:19 +0100

    version 1.0.2clear

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='hatchbuck',
-      version='1.0.1',
+      version='1.0.2',
       description='Hatchbuck.com CRM API bindings for python',
       long_description=long_description,
       long_description_content_type='text/markdown',
