d00775c55dc6558aab7c303d109d145ffb19b5ec
BasharSaid <33035384+basharSaid@users.noreply.github.com>
2018-03-27 16:33:53 +0200

    search_multiple_email_addresses.py

--- examples/search_multiple_email_addresses.py
+++ examples/search_multiple_email_addresses.py
@@ -4,6 +4,6 @@ sys.path.append('..')
 from hatchbuck import Hatchbuck
 import pprint
 pp = pprint.PrettyPrinter()
-hatchbuck = Hatchbuck('cE91YTNnQ3NBWVRaaXlERHBKR1NaZjJyTTFKa1BrRXlQYjZQVjVuLVpSSTE1')
+hatchbuck = Hatchbuck(sys.argv[1])
 profile = hatchbuck.search_email_multi(['sgdhfgfdgh@fdvd.com', 'bashar.said@vshn.ch'])
 pp.pprint(profile)
