04005e4f93bf711b1f42762aa685426e09f1a6ed
BasharSaid <33035384+basharSaid@users.noreply.github.com>
2018-05-04 10:59:39 +0200

    travis configuration

--- setup.py
+++ setup.py
@@ -1,26 +1,29 @@
+from os.path import abspath, dirname, join
 from setuptools import setup
-from os import path
 
-here = path.abspath(path.dirname(__file__))
+__version__ = '1.0.4'
 
-# Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+
+def read_file(filename):
+    """Get the contents of a file"""
+    here = abspath(dirname(__file__))
+    with open(join(here, filename), encoding='utf-8') as f:
+        return f.read()
+
 
 setup(name='hatchbuck',
-      version='1.0.4',
-      description='Hatchbuck.com CRM API bindings for python',
-      long_description=long_description,
+      version=__version__,
+      description='Hatchbuck.com CRM API bindings for Python',
+      long_description=read_file('README.rst'),
       keywords='hatchbuck CRM API',
       url='https://github.com/vshn/python-hatchbuck',
       author='VSHN AG, Bashar Said',
       author_email='bashar.said@vshn.ch',
       # BSD 3-Clause License:
       # - http://opensource.org/licenses/BSD-3-Clause
-      license = 'BSD',
+      license='BSD',
       py_modules=["hatchbuck"],
-      install_requires = [
-            'requests>=2',
-            'pycountry>=1',
-      ],
-)
+      install_requires=[
+          'requests>=2',
+          'pycountry>=1',
+      ])
