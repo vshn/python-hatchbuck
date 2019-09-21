dd57c38af35dcaad3a66008f223944abf094e392
Aarno Aukia <aarno.aukia@vshn.ch>
2019-09-22 02:53:21 +0300

    add pylint

--- examples/profile_contains.py
+++ examples/profile_contains.py
@@ -5,7 +5,5 @@ import sys
 pp = pprint.PrettyPrinter()
 hatchbuck = Hatchbuck(sys.argv[1])
 profile = hatchbuck.search_email("bashar.said@vshn.ch")
-profile = hatchbuck.profile_contains(
-    profile, "phones", "number", "+(414) 454-5 53 00"
-)
+profile = hatchbuck.profile_contains(profile, "phones", "number", "+(414) 454-5 53 00")
 pp.pprint(profile)
--- examples/search_multiple_email_addresses.py
+++ examples/search_multiple_email_addresses.py
@@ -4,7 +4,5 @@ import sys
 
 pp = pprint.PrettyPrinter()
 hatchbuck = Hatchbuck(sys.argv[1])
-profile = hatchbuck.search_email_multi(
-    ["sgdhfgfdgh@fdvd.com", "bashar.said@vshn.ch"]
-)
+profile = hatchbuck.search_email_multi(["sgdhfgfdgh@fdvd.com", "bashar.said@vshn.ch"])
 pp.pprint(profile)
